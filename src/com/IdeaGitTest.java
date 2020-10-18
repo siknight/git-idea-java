980af56f138f88fdd5426c9a17db0f049fd80d00
lisi <1786678583@qq.com>
2020-10-18 17:01:31 +0800

    3

--- src/com/IdeaGitTest.java
+++ src/com/IdeaGitTest.java
@@ -4,5 +4,6 @@ public class IdeaGitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println(2);
+        System.out.println(3);
     }
 }
